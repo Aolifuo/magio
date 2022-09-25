304e7a192a82b5175759b6fa7172bf2fa6b1a2a2
Mikasuki <2649856056@qq.com>
2022-09-25 15:12:35 +0800

    add context

--- src/magio/workflow/Workflow.h
+++ src/magio/workflow/Workflow.h
@@ -7,11 +7,43 @@
 
 namespace magio {
 
+class Context;
+
+using ContextPtr = std::shared_ptr<Context>;
+using TaskHandler = std::function<void(ContextPtr)>;
+
+class Context {
+public:
+    Context(AnyExecutor exe, std::function<void(std::exception_ptr)>&& eh)
+        : executor_(exe)
+        , error_handler_(std::move(eh))
+    {
+
+    }
+
+    void post(CompletionHandler&& handler) {
+        executor_.post(std::move(handler));
+    }
+
+    void set_timeout(size_t ms, CompletionHandler&& handler) {
+        executor_.set_timeout(ms, std::move(handler));
+    }
+
+    void handle_exception() {
+        error_handler_(std::current_exception());
+    }
+private:
+    SpinLock spin_;
+    AnyExecutor executor_;
+    
+    std::function<void(std::exception_ptr)> error_handler_;
+};
+
 class TaskNode: public std::enable_shared_from_this<TaskNode> {
 
     friend class Workflow;
 public:
-    TaskNode(CompletionHandler&& handler)
+    TaskNode(TaskHandler&& handler)
         : handler_(std::move(handler))
     {
 
@@ -31,49 +63,55 @@ public:
         (nodes->pre_nums_++, ...);
     }
 
-    virtual void invoke(AnyExecutor executor) {
-        handler_();
-        invoke_successors(executor);
+    virtual void invoke(const ContextPtr& ctx) {
+        ctx->post([p = shared_from_this(), ctx] {
+            try {
+                p->handler_(ctx);
+                p->invoke_successors(ctx);
+            } catch(...) {
+                ctx->handle_exception();
+            }
+        });
     }
 
 protected:
-    void invoke_successors(AnyExecutor executor) {
+    void invoke_successors(const ContextPtr& ctx) {
         for (auto& succ : successors_) {
-            succ->try_invoke(executor);
+            succ->try_invoke(ctx);
         }
     }
 
-    void try_invoke(AnyExecutor executor) {
-        std::lock_guard lk(spin_);
-        --pre_nums_;
-        if (pre_nums_ == 0) {
-            executor.post([p = shared_from_this(), executor] {
-                p->invoke(executor);
-            });
+    void try_invoke(const ContextPtr& ctx) {
+        {
+            std::lock_guard lk(spin_);
+            --pre_nums_;
+            if (pre_nums_ != 0) {
+                return;
+            }
         }
+        invoke(ctx);
     }
 
     SpinLock spin_;
     size_t pre_nums_;
 
-    CompletionHandler handler_;
-    std::exception_ptr eptr_;
+    TaskHandler handler_;
 
     std::vector<std::shared_ptr<TaskNode>> successors_;
 };
 
 class TimedTask: public TaskNode {
 public:
-    TimedTask(size_t ms, CompletionHandler&& handler)
+    TimedTask(size_t ms, TaskHandler&& handler)
         : TaskNode(std::move(handler))
         , ms_(ms)
     {
 
     }
 
-    void invoke(AnyExecutor executor) override {
-        executor.set_timeout(ms_, [p = shared_from_this(), executor] {
-            p->TaskNode::invoke(executor);
+    void invoke(const ContextPtr& ctx) override {
+        ctx->set_timeout(ms_, [p = shared_from_this(), ctx] {
+            p->TaskNode::invoke(ctx);
         });
     }
 private:
@@ -87,19 +125,17 @@ public:
         (executor.post([fns = std::forward<Fns>(fns)] { fns(); }), ...);
     }
 
-    static std::shared_ptr<TimedTask> create_timed_task(size_t ms, CompletionHandler&& handler) {
+    static std::shared_ptr<TimedTask> create_timed_task(size_t ms, TaskHandler&& handler) {
         return std::make_shared<TimedTask>(ms, std::move(handler));
     }
 
-    static std::shared_ptr<TaskNode> create_task(CompletionHandler&& handler) {
+    static std::shared_ptr<TaskNode> create_task(TaskHandler&& handler) {
         return std::make_shared<TaskNode>(std::move(handler));
     }
 
-    static void run(AnyExecutor executor, std::shared_ptr<TaskNode> node) {
-        node->invoke(executor);
-        if (node->eptr_) {
-            std::rethrow_exception(node->eptr_);
-        }
+    static void run(AnyExecutor executor, std::shared_ptr<TaskNode> node, std::function<void(std::exception_ptr)>&& error_handler) {
+        auto ctx = std::make_shared<Context>(executor, std::move(error_handler));
+        node->invoke(ctx);
     }
 private:
 };
