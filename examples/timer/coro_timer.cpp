451c81a21760bb550041390247abbc223be7e460
Mikasuki <2649856056@qq.com>
2022-09-27 17:41:06 +0800

    update

--- examples/timer/coro_timer.cpp
+++ examples/timer/coro_timer.cpp
@@ -4,19 +4,21 @@
 #include "magio/coro/Coro.h"
 #include "magio/coro/ThisCoro.h"
 
-magio::Coro<void> amain() {
-    auto executor = co_await magio::this_coro::executor;
-    magio::Timer timer(executor, 2000);
+using namespace magio;
+using namespace std;
+ 
+Coro<void> amain() {
+    Timer timer(co_await this_coro::executor, 1000);
 
-    std::printf("Hello ");
-    co_await timer.async_wait(magio::use_coro);
-    std::printf("world\n");
+    printf("Hello ");
+    co_await timer.async_wait(use_coro);
+    printf("world\n");
 }
 
 int main() {
-    magio::EventLoop loop;
+    EventLoop loop;
 
-    magio::co_spawn(loop.get_executor(), amain(), magio::detached);
+    co_spawn(loop.get_executor(), amain(), detached);
 
     loop.run();
 }
