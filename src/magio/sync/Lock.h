587d8afaa7560561bfeed36a08853e87d95ab9fa
Mikasuki <2649856056@qq.com>
2022-09-30 15:39:01 +0800

    add try_lock

--- src/magio/sync/Lock.h
+++ src/magio/sync/Lock.h
@@ -14,6 +14,10 @@ public:
         while (flag_.test_and_set(std::memory_order_acquire));
     }
 
+    bool try_lock() {
+        return flag_.test_and_set(std::memory_order_acquire);
+    }
+
     void unlock() {
         flag_.clear(std::memory_order_release);
     }
