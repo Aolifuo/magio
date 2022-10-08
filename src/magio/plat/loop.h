62b6eaf47c4fd2c60f88ce9098a9f4a84002d3f3
Mikasuki <2649856056@qq.com>
2022-10-08 17:55:45 +0800

    support iocp

--- src/magio/plat/loop.h
+++ src/magio/plat/loop.h
@@ -1,18 +1,27 @@
 #pragma once
 
 #ifdef __linux__
-
 #include <netinet/in.h>
+#include "magio/plat/epoll/epoll.h"
+#endif
+
+#ifdef _WIN32
+#include "magio/plat/iocp/iocp.h"
+#include <Ws2tcpip.h>
+#include <MSWSock.h>
+#endif
+
 #include "magio/dev/Log.h"
 #include "magio/core/Queue.h"
 #include "magio/plat/io.h"
 #include "magio/plat/declare.h"
 #include "magio/plat/errors.h"
-#include "magio/plat/epoll/epoll.h"
+
 
 namespace magio {
 
 namespace plat {
+#ifdef __linux__
 
 struct EventData {
     int     fd;
@@ -69,6 +78,7 @@ public:
 
     void async_accept(EventData* data) {
         DEBUG_LOG("async_accept");
+        data->op = IOOP::Accept;
         if (!conn_fds_.empty()) {
             int fd = conn_fds_.front();
             conn_fds_.pop();
@@ -78,27 +88,30 @@ public:
         }
     }
 
-    void async_connect(int fd, EventData* data) {
+    void async_connect(EventData* data) {
         DEBUG_LOG("async_connect");
-        auto ec = update_epoll(EPOLL_CTL_ADD, EPOLLOUT, fd, data);
+        data->op = IOOP::Connect;
+        auto ec = update_epoll(EPOLL_CTL_ADD, EPOLLOUT, data->fd, data);
         if (ec) {
             data->cb(ec, data->data, fd);
         }
     }
 
-    void async_receive(int fd, EventData* data) {
+    void async_receive(EventData* data) {
         DEBUG_LOG("async_receive");
-        auto ec = update_epoll(EPOLL_CTL_MOD, EPOLLIN, fd, data);
+        data->op = IOOP::Receive;
+        auto ec = update_epoll(EPOLL_CTL_MOD, EPOLLIN, data->fd, data);
         if (ec) {
-            data->cb(ec, data->data, fd);
+            data->cb(ec, data->data, data->fd);
         }
     }
 
-    void async_send(int fd, EventData* data) {
+    void async_send(EventData* data) {
         DEBUG_LOG("async_send");
-        auto ec = update_epoll(EPOLL_CTL_MOD, EPOLLOUT, fd, data);
+        data->op = IOOP::Send;
+        auto ec = update_epoll(EPOLL_CTL_MOD, EPOLLOUT, data->fd, data);
         if (ec) {
-            data->cb(ec, data->data, fd);
+            data->cb(ec, data->data, data->fd);
         }
     }
 
@@ -261,8 +274,218 @@ private:
     RingQueue<int>              conn_fds_;
 };
 
-}
+#endif
 
-}
+#ifdef _WIN32
+
+class IOLoop {
+    IOLoop(IOCompletionPort&& iocp, void* accpet_ex, void* connect_ex)
+        : iocp_(std::move(iocp))
+        , accept_ex_((LPFN_ACCEPTEX)accpet_ex)
+        , connect_ex_((LPFN_CONNECTEX)connect_ex)
+    { }
+public:
+    IOLoop(IOLoop&& other) noexcept = default;
+    IOLoop& operator=(IOLoop&& other) noexcept = default;
+
+    static Expected<IOLoop> create(SOCKET sock) {
+        GUID GuidAcceptEx = WSAID_ACCEPTEX;
+        DWORD bytes_return;
+        void* accept_ex_ptr = nullptr;
+        if (0 != ::WSAIoctl(
+            sock, 
+            SIO_GET_EXTENSION_FUNCTION_POINTER, 
+            &GuidAcceptEx, 
+            sizeof(GuidAcceptEx), 
+            &accept_ex_ptr, 
+            sizeof(accept_ex_ptr), 
+            &bytes_return, 
+            NULL, 
+            NULL))
+        {
+            return SYSTEM_ERROR;
+        }
+
+        GUID GUidConnectEx = WSAID_CONNECTEX;
+        void* connect_ex_ptr = nullptr;
+        if (0 != ::WSAIoctl(
+            sock, 
+            SIO_GET_EXTENSION_FUNCTION_POINTER, 
+            &GUidConnectEx, 
+            sizeof(GUidConnectEx), 
+            &connect_ex_ptr, 
+            sizeof(connect_ex_ptr), 
+            &bytes_return, 
+            NULL, 
+            NULL))
+        {
+            return SYSTEM_ERROR;
+        }
+
+        auto iocp = IOCompletionPort::create();
+        if (!iocp) {
+            return iocp.unwrap_err();
+        }
+        return IOLoop{iocp.unwrap(), accept_ex_ptr, connect_ex_ptr};
+    }
+
+    std::error_code relate(SOCKET fd) {
+        return iocp_.relate(fd);
+    }
+
+    void async_accept(SOCKET fd, IOData* io) {
+        io->op = IOOP::Accept;
+        ZeroMemory(&io->overlapped, sizeof(OVERLAPPED));
+
+        bool status = accept_ex_(
+            fd,
+            io->fd,
+            io->input_buffer.buf,
+            0,
+            sizeof(sockaddr_in) + 16,
+            sizeof(sockaddr_in) + 16,
+            NULL,
+            (LPOVERLAPPED)&io->overlapped
+        );
+
+        if (!status && ERROR_IO_PENDING != ::GetLastError()) {
+            io->cb(SYSTEM_ERROR, io->ptr, fd);
+            return;
+        }
+    }
+
+    void async_connect(IOData* io, const char* host, uint_least16_t port) {
+        io->op = IOOP::Connect;
+        ZeroMemory(&io->overlapped, sizeof(OVERLAPPED));
+
+        // 远端
+        sockaddr_in remote_addr{};
+        remote_addr.sin_family = AF_INET;
+        remote_addr.sin_port = ::htons(port);
+        if (-1 == ::inet_pton(AF_INET, host, &remote_addr.sin_addr)) {
+            io->cb(SYSTEM_ERROR, io->ptr, io->fd);
+            return;
+        }
+
+        // data->fd target
+        bool status = connect_ex_(
+            io->fd,
+            (sockaddr*)&remote_addr,
+            sizeof(sockaddr),
+            NULL,
+            0,
+            NULL,
+            (LPOVERLAPPED)&io->overlapped
+        );
+
+        if (!status && ERROR_IO_PENDING != ::GetLastError()) {
+            io->cb(SYSTEM_ERROR, io->ptr, io->fd);
+        }
+
+    }
+
+    void async_receive(IOData* io) {
+        io->op = IOOP::Receive;
+        ZeroMemory(&io->overlapped, sizeof(OVERLAPPED));
+        io->wsa_buf.buf = io->input_buffer.buf;
+        io->wsa_buf.len = global_config.buffer_size;
+
+        DWORD flag = 0;
+        int status = ::WSARecv(
+            io->fd, 
+            (LPWSABUF)&io->wsa_buf, 
+            1, 
+            NULL,
+            &flag, 
+            (LPOVERLAPPED)&io->overlapped, 
+            NULL);
+    
+        if (SOCKET_ERROR == status && ERROR_IO_PENDING != ::GetLastError()) {
+            io->cb(SYSTEM_ERROR, io->ptr, io->fd);
+        }
+    }
+
+    void async_send(IOData* io) {
+        io->op = IOOP::Send;
+        ZeroMemory(&io->overlapped, sizeof(OVERLAPPED));
+        io->wsa_buf.buf = io->output_buffer.buf;
+        io->wsa_buf.len = (ULONG)io->output_buffer.len;
+
+        DWORD flag = 0;
+        int status = ::WSASend(
+            io->fd, 
+            &io->wsa_buf, 
+            1, 
+            NULL, 
+            flag, 
+            (LPOVERLAPPED)&io->overlapped, 
+            NULL);
+        
+        if (SOCKET_ERROR == status && ERROR_IO_PENDING != ::GetLastError()) {
+            io->cb(SYSTEM_ERROR, io->ptr, io->fd);
+        }
+    }
+
+    std::error_code poll(size_t timeout) {
+        DWORD bytes_transferred = 0;
+        IOData* iodata = nullptr;
+
+        auto ec = iocp_.wait(timeout, &bytes_transferred, (void**)&iodata);
+        if (ec.value() == WAIT_TIMEOUT) {
+            return {};
+        }
+
+        if (!iodata) {
+            return ec;
+        }
+
+        switch (iodata->op) {
+        case IOOP::Accept: {
+            DEBUG_LOG("do accept");
+            iodata->cb(ec, iodata->ptr, iodata->fd);
+            break;
+        } 
+        case IOOP::Connect: {
+            DEBUG_LOG("do connect");
+            iodata->cb(ec, iodata->ptr, iodata->fd);
+            break;
+        }
+        case IOOP::Receive:
+            DEBUG_LOG("do receive");
+            iodata->input_buffer.len = bytes_transferred;
+            if (bytes_transferred == 0) {
+                ec = make_win_system_error(-1);
+            }
+            iodata->cb(ec, iodata->ptr, iodata->fd);
+            break;
+        case IOOP::Send:
+            DEBUG_LOG("do send");
+            iodata->output_buffer.len = bytes_transferred;
+            if (bytes_transferred == 0) {
+                ec = make_win_system_error(-1);
+            }
+            iodata->cb(ec, iodata->ptr, iodata->fd);
+            break;
+        default:
+            break;
+        }
+
+        return {};
+    }
+
+    void close() {
+        iocp_.close();
+    }
+private:
+    IOCompletionPort                            iocp_;
+
+    LPFN_ACCEPTEX                               accept_ex_;
+    LPFN_CONNECTEX                              connect_ex_;
+};
 
 #endif
+
+}
+
+}
+
