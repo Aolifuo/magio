f23d07535623f1c54124ab58988075892a2a0394
Mikasuki <2649856056@qq.com>
2022-10-10 18:00:19 +0800

    cpp impl coroutines

--- src/magio/coro/Config.h
+++ src/magio/coro/Config.h
@@ -1,13 +1,22 @@
 #pragma once
 
+#ifdef __cpp_impl_coroutine
+#include <coroutine>
+#else
 #include <experimental/coroutine>
+#endif
 
 namespace magio {
-
+#ifdef __cpp_impl_coroutine
+using std::coroutine_handle;
+using std::coroutine_traits;
+using std::suspend_always;
+using std::suspend_never;
+#else
 using std::experimental::coroutine_handle;
 using std::experimental::coroutine_traits;
 using std::experimental::suspend_always;
 using std::experimental::suspend_never;
-
+#endif
 
 }
