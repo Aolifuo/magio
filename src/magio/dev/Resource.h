f1755535b18bda129c1a446af257d769544901d6
Mikasuki <2649856056@qq.com>
2022-10-10 18:02:05 +0800

    remove buffer checker

--- src/magio/dev/Resource.h
+++ src/magio/dev/Resource.h
@@ -12,34 +12,22 @@ inline std::atomic_int SOCKET_NUMS = 0;
 inline std::atomic_int BUFFER_NUMS = 0;
 
 #ifdef MAGIO_DEBUG
-    #define MAGIO_MAKE_SOCKET (++dev::SOCKET_NUMS)
-    #define MAGIO_CLOSE_SOCKET (--dev::SOCKET_NUMS)
-    #define MAGIO_GET_BUFFER (++dev::BUFFER_NUMS)
-    #define MAGIO_PUT_BUFFER (--dev::BUFFER_NUMS)
-
-class ResourceChecker {
-public:
-    ~ResourceChecker() {
-        if (SOCKET_NUMS > 0) {
-            DEBUG_LOG("socket leak");
-        }
-
-        if (BUFFER_NUMS > 0) {
-            DEBUG_LOG("buffer leak");
-        }
-
-        DEBUG_LOG("check end");
-    }
-private:
-};
-
-inline ResourceChecker resource_checker;
+#define MAGIO_MAKE_SOCKET (++dev::SOCKET_NUMS)
+#define MAGIO_CLOSE_SOCKET (--dev::SOCKET_NUMS)
+#define MAGIO_CHECK_RESOURCE \
+    do { \
+        if (magio::dev::SOCKET_NUMS > 0) { \
+            DEBUG_LOG("socket leak ", magio::dev::SOCKET_NUMS); \
+        } \
+        DEBUG_LOG("check end"); \
+    } while(0)
 
 #else
-    #define MAGIO_MAKE_SOCKET 
-    #define MAGIO_CLOSE_SOCKET 
-    #define MAGIO_GET_BUFFER 
-    #define MAGIO_PUT_BUFFER 
+#define MAGIO_MAKE_SOCKET 
+#define MAGIO_CLOSE_SOCKET 
+#define MAGIO_GET_BUFFER 
+#define MAGIO_PUT_BUFFER
+#define MAGIO_CHECK_RESOURCE
 #endif
 
 }
