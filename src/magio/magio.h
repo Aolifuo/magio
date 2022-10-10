7dbed48d182878008c7401288f4151b11d39b5ec
Mikasuki <2649856056@qq.com>
2022-10-10 17:58:16 +0800

    add magio.h

--- src/magio/magio.h
+++ src/magio/magio.h
@@ -1,12 +1,14 @@
 #pragma once
 
-#include "magio/Configs.h"
-#include "magio/ThisContext.h"
+#include "magio/EventLoop.h"
 #include "magio/ThreadPool.h"
 #include "magio/coro/CoSpawn.h"
 #include "magio/coro/Operation.h"
 #include "magio/timer/Timer.h"
 #include "magio/workflow/Workflow.h"
+#include "magio/net/SocketAddress.h"
 #include "magio/net/Tcp.h"
+#include "magio/net/Udp.h"
 #include "magio/sync/Channel.h"
 #include "magio/sync/WaitGroup.h"
+#include "magio/dev/Resource.h"
