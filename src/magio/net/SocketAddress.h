e60c84a96eef7d1f8777456d84623af397a5ae2a
aolifuo <aolifuo@qq.com>
2022-10-12 21:54:29 +0800

    add make address

--- src/magio/net/SocketAddress.h
+++ src/magio/net/SocketAddress.h
@@ -1,6 +1,12 @@
 #pragma once
 
 #include <string>
+#include "magio/core/Error.h"
+#include "magio/plat/errors.h"
+
+#ifdef __linux__
+#include <arpa/inet.h>
+#endif
 
 #ifdef _WIN32
 #include <WinSock2.h>
@@ -17,8 +23,16 @@ struct SocketAddress {
     std::string to_string() {
         return ip + ":" + std::to_string(host);
     }
-
-    
 };
+
+inline Expected<SocketAddress> make_address(const char* ip, uint_least16_t port) {
+    sockaddr_in addr{};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    if (-1 == ::inet_pton(AF_INET, ip, &addr.sin_addr)) {
+        return MAGIO_SYSTEM_ERROR;
+    }
+    return SocketAddress{ip, port, addr};
+}
 
 }
