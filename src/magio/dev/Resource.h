b683b37fde1d19a17075f2556b143ddfeeb79445
aolifuo <aolifuo@qq.com>
2022-10-12 21:53:14 +0800

    add coro leak checker

--- src/magio/dev/Resource.h
+++ src/magio/dev/Resource.h
@@ -9,15 +9,20 @@ namespace magio {
 namespace dev {
 
 inline std::atomic_int SOCKET_NUMS = 0;
-inline std::atomic_int BUFFER_NUMS = 0;
+inline std::atomic_int CORO_NUMS = 0;
 
 #ifdef MAGIO_DEBUG
 #define MAGIO_MAKE_SOCKET (++dev::SOCKET_NUMS)
 #define MAGIO_CLOSE_SOCKET (--dev::SOCKET_NUMS)
+#define MAGIO_CREATE_CORO (++dev::CORO_NUMS)
+#define MAGIO_DESTROY_CORO (--dev::CORO_NUMS)
 #define MAGIO_CHECK_RESOURCE \
     do { \
         if (magio::dev::SOCKET_NUMS > 0) { \
             DEBUG_LOG("socket leak ", magio::dev::SOCKET_NUMS); \
+        } \
+        if (magio::dev::CORO_NUMS > 0) { \
+            DEBUG_LOG("coro leak ", magio::dev::CORO_NUMS); \
         } \
         DEBUG_LOG("check end"); \
     } while(0)
@@ -25,8 +30,8 @@ inline std::atomic_int BUFFER_NUMS = 0;
 #else
 #define MAGIO_MAKE_SOCKET 
 #define MAGIO_CLOSE_SOCKET 
-#define MAGIO_GET_BUFFER 
-#define MAGIO_PUT_BUFFER
+#define MAGIO_CREATE_CORO
+#define MAGIO_DESTROY_CORO
 #define MAGIO_CHECK_RESOURCE
 #endif
 
