fd83a6328432506a733684ce0f4c729827df5061
Mikasuki <2649856056@qq.com>
2022-10-13 22:44:24 +0800

    add awaitable and waker

---
+++ src/magio/coro/Awaitbale.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "magio/coro/PromiseBase.h"
+#include "magio/execution/Execution.h"
+
+namespace magio {
+
+class Waker {
+public:
+    Waker() = default;
+
+    Waker(PromiseNode* node)
+        : node_(node)
+    { }
+
+    void try_wake() const {
+        if (node_->stop_flag_->test()) {
+            node_->destroy_from_tail();
+        } else {
+            node_->wake();
+        }
+    }
+private:
+    PromiseNode* node_ = nullptr;
+};
+
+using WakeHandler = std::function<void(AnyExecutor, Waker waker)>;
+
+class Awaitable {
+public:
+    Awaitable(WakeHandler&& handle)
+        : waker_handler_(std::move(handle)) 
+    { }
+
+    Awaitable(const Awaitable&) = delete;
+
+    bool await_ready() { return false; }
+
+    template<typename PT>
+    void await_suspend(coroutine_handle<PT> prev_h) {
+        waker_handler_(prev_h.promise().executor_, &prev_h.promise());
+    }
+
+    void await_resume() {
+
+    }
+private:
+    WakeHandler waker_handler_;
+};
+
+}
