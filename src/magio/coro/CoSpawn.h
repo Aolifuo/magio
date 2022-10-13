92b92a9e9151b5159101735315a10d51c9683e3a
Mikasuki <2649856056@qq.com>
2022-10-13 22:50:12 +0800

    change waker and fix use_future bugs

--- src/magio/coro/CoSpawn.h
+++ src/magio/coro/CoSpawn.h
@@ -6,36 +6,40 @@
 namespace magio {
 
 template<typename Ret>
-inline void co_spawn(AnyExecutor executor, Coro<Ret>&& coro, detail::Detached d = {}) {
-    coro.wake(executor, true);
+inline void co_spawn(AnyExecutor executor, Coro<Ret> coro, detail::Detached d = {}) {
+    coro.wake(executor);
 }
 
 template<typename Ret>
-inline void co_spawn(AnyExecutor executor, Coro<Ret>&& coro, CoroHandler<Ret> handler) {
+inline void co_spawn(AnyExecutor executor, Coro<Ret> coro, CoroHandler<Ret> handler) {
     coro.set_completion_handler(std::move(handler));
-    coro.wake(executor, true);
+    coro.wake(executor);
 }
 
 template<typename Ret>
-inline Coro<Ret> co_spawn(AnyExecutor executor, Coro<Ret>&& coro, detail::UseCoro) {
+inline Coro<Ret> co_spawn(AnyExecutor executor, Coro<Ret> coro, detail::UseCoro) {
     co_return co_await coro;
 }
 
 template<typename Ret>
-inline std::future<Ret> co_spawn(AnyExecutor executor, Coro<Ret>&& coro, detail::UseFuture) {
+inline std::future<Ret> co_spawn(AnyExecutor executor, Coro<Ret> coro, detail::UseFuture) {
     auto pro_ptr = std::make_shared<std::promise<Ret>>();
     auto fut = pro_ptr->get_future();
     coro.set_completion_handler(
-        [pro_ptr](Expected<Ret, std::exception_ptr> exp) mutable {
+        [pro_ptr](Expected<util::VoidToUnit<Ret>, std::exception_ptr> exp) mutable {
             if (exp) {
-                pro_ptr->set_value(exp.unwrap());
+                if constexpr (std::is_void_v<Ret>) {
+                    pro_ptr->set_value();
+                } else {
+                    pro_ptr->set_value(exp.unwrap());
+                }
             } else {
                 pro_ptr->set_exception(exp.unwrap_err());
             }
         }
     );
 
-    coro.wake(executor, true);
+    coro.wake(executor);
     return fut;
 }
 
--- src/magio/coro/Coro.h
+++ src/magio/coro/Coro.h
@@ -1,7 +1,8 @@
 #pragma once
 
-#include "magio/coro/UseCoro.h"
-#include "magio/utils/ScopeGuard.h"
+#include "magio/coro/PromiseBase.h"
+#include "magio/coro/Awaitbale.h"
+#include "magio/execution/Execution.h"
 
 #ifdef __cpp_impl_coroutine
 template<typename Ret, typename...Ts>
@@ -17,8 +18,14 @@ struct std::experimental::coroutine_traits<magio::Coro<Ret>, Ts...> {
 };
 #endif
 
-
 namespace magio {
+
+namespace util {
+
+template<typename T>
+using VoidToUnit = std::conditional_t<std::is_void_v<T>, Unit, T>;
+
+}
 
 namespace this_coro {
 
@@ -27,7 +34,7 @@ struct GetExecutor {
 
     template<typename PT>
     auto await_suspend(coroutine_handle<PT> prev_h) {
-        executor = prev_h.promise().executor;
+        executor = prev_h.promise().executor_;
         prev_h.resume();
     }
 
@@ -42,18 +49,92 @@ inline GetExecutor executor;
 
 }
 
-inline Coro<> timeout(size_t ms) {
-    auto exe = co_await this_coro::executor;
-    co_await Coro<>{
-        [exe, ms](coroutine_handle<> h) mutable {
-            exe.set_timeout(ms, [h]() mutable {
-                h.resume();
+template<typename Ret = void>
+class Coro {
+public:
+    Coro(coroutine_handle<PromiseTypeBase<Ret, Coro>> h)
+        : main_h_(h)
+    { }
+
+    ~Coro() {
+        if (!main_h_) {
+            return;
+        }
+
+        // the coro that not start
+        if (main_h_.promise().stop_flag_ == nullptr) {
+            main_h_.promise().destroy();
+        }
+    }
+
+    Coro(const Coro&) = delete;
+
+    Coro(Coro&& other) noexcept {
+        main_h_ = other.main_h_;
+        other.main_h_ = {};
+    }
+    Coro& operator=(Coro&& other) noexcept {
+        main_h_ = other.main_h_;
+        other.main_h_ = {};
+        return *this;
+    }
+
+    bool await_ready() { return false; }
+
+    template<typename PT>
+    void await_suspend(coroutine_handle<PT> prev_h) {
+        main_h_.promise().executor_ = prev_h.promise().executor_;
+
+        prev_h.promise().next_ = &main_h_.promise();
+        main_h_.promise().prev_ = &prev_h.promise();
+
+        main_h_.promise().stop_flag_ = prev_h.promise().stop_flag_;
+
+        main_h_.promise().wake();
+    }
+
+    Ret await_resume() {
+        if constexpr(!std::is_void_v<Ret>) {
+            return main_h_.promise().get_value();
+        } else {
+            main_h_.promise().get_value();
+        }
+    }
+
+    void set_completion_handler(CoroHandler<Ret>&& handler) {
+        main_h_.promise().completion_handler_ = std::move(handler);
+    }
+
+    void wake(AnyExecutor executor) {
+        main_h_.promise().stop_flag_ = std::make_shared<std::atomic_flag>();
+        main_h_.promise().executor_ = executor;
+        executor.post([main_h_ = main_h_] { main_h_.promise().wake(); });
+    }
+
+    void stop() {
+        main_h_.promise().stop_flag_->test_and_set();
+    }
+
+    auto& promise() const {
+        return main_h_.promise();
+    }
+
+private:
+    coroutine_handle<PromiseTypeBase<Ret, Coro>> main_h_;
+};
+
+inline Coro<> sleep(size_t ms) {
+    co_await Awaitable{
+        [ms](AnyExecutor exe, Waker waker) {
+            exe.set_timeout(ms, [waker] {
+                waker.try_wake();
             });
         }
     };
 }
 
 }
+
 
 
 
