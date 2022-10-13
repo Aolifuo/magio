74e9976934b842966bae9ae49b0e63f64b1dfc25
Mikasuki <2649856056@qq.com>
2022-10-13 22:42:13 +0800

    use operators

--- examples/coro_join.cpp
+++ examples/coro_join.cpp
@@ -2,13 +2,14 @@
 
 using namespace std;
 using namespace magio;
+using namespace operators;
 
 Coro<int> factorial(std::string_view name, int num) {
     int res = 1;
 
     for (int i = 2; i <= num; ++i) {
         printf("Task %s: Compute factorial %d, now i = %d\n", name.data(), num, i);
-        co_await timeout(1000);
+        co_await sleep(1000);
         res *= i;
     }
     printf("Task %s: factorial %d = %d\n", name.data(), num, res);
@@ -20,9 +21,9 @@ int main() {
     co_spawn(
         loop.get_executor(),
         []() -> Coro<> {
-            auto [a, b, c] = co_await coro_join(
-                factorial("A", 2),
-                factorial("B", 3),
+            auto [a, b, c] = co_await(
+                factorial("A", 2) &&
+                factorial("B", 3) &&
                 factorial("C", 4)
             );
             printf("%d %d %d\n", a, b, c);
