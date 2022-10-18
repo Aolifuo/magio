2249fbe60eb1646575081398da68b075272d123a
Mikasuki <2649856056@qq.com>
2022-10-18 20:59:23 +0800

    update: add tests for new method of thread pool

--- tests/test_thread_pool.cpp
+++ tests/test_thread_pool.cpp
@@ -1,30 +1,28 @@
+#include "magio/magio.h"
 #include "preload.h"
-#include "magio/ThreadPool.h"
-#include "magio/sync/WaitGroup.h"
 
-size_t sum_from_to(size_t frist, size_t last) {
+size_t sum_from_to(size_t first, size_t last) {
     size_t res = 0;
-    for (size_t i = frist; i <= last; ++i) {
+    for (size_t i = first; i <= last; ++i) {
         res += i;
     }
     return res;
 }
 
-TestResults test_post() {
+TestResults test_wake() {
     ThreadPool pool(8);
     
     auto waiter = make_shared<WaitGroup>(100);
     atomic_size_t sum = 0;
 
     for (int i = 0; i < 100; ++i) {
-        pool.post([&sum, waiter] {
-            sum += sum_from_to(1, 100);
+        co_spawn(pool.get_executor(), [&sum, waiter] {
+            sum += sum_from_to(0, 100);
             waiter->done();
         });
     }
 
     waiter->wait();
-
     TESTCASE(
         test(sum.load(), 505000)
     );
@@ -37,7 +35,10 @@ TestResults test_future() {
     res_vec.reserve(100);
     int a = 1;
     for (int i = 0; i < 100; ++i) {
-        res_vec.emplace_back(pool.get_future(sum_from_to, a, 100));
+        auto fut = co_spawn(pool.get_executor(), [a]()->Coro<size_t> {
+            co_return sum_from_to(a, 100);
+        }(), use_future);
+        res_vec.emplace_back(std::move(fut));
     }
     
     size_t sum = 0;
@@ -55,21 +56,22 @@ TestResults test_copy_times() {
 
     PrintCopy cp;
 
-    pool.get_future([](PrintCopy pc) {
+    co_spawn(pool.get_executor(), [](PrintCopy pc)->Coro<> {
+        co_return;
+    }(PrintCopy{}), use_future).get();
 
-    }, PrintCopy{});
-
-    pool.get_future([](PrintCopy& rpc) {
-
-    }, std::ref(cp));
+    co_spawn(pool.get_executor(), [](PrintCopy& pc)->Coro<> {
+        co_return;
+    }(cp), use_future).get();
 
     TESTCASE(
-        test(PrintCopy::cp_times, 1, "copy times error")
+        test(PrintCopy::cp_times, 0, "copy times error")
     );
 }
 
 int main() {
     TESTALL(
+        test_wake(),
         test_future(),
         test_copy_times()
     );
