35f600fd87f856fe510e7929789b53e16d2e059e
Mikasuki <2649856056@qq.com>
2022-10-18 20:42:41 +0800

    add: udp server with timeout cancellation

--- examples/udp-echo.cpp
+++ examples/udp-echo.cpp
@@ -5,23 +5,28 @@ using namespace std;
 using namespace magio;
 
 Coro<> amain() {
-    auto exe = co_await this_coro::executor;
-    auto socket = co_await UdpSocket::bind("127.0.0.1", 8001);
-    auto channel = make_shared<Channel<string_view, SocketAddress>>(exe);
-
-    // send
-    co_spawn(exe, [&socket, channel]() -> Coro<> {
+    try {
+        array<char, 1024> buf;
+        auto socket = co_await UdpSocket::bind("127.0.0.1", 8001);
         for (; ;) {
-            auto [str, addr] = co_await channel->async_receive(use_coro);
-            co_await socket.write_to(str.data(), str.length(), addr);
-        }
-    }());
+            // 5秒后超时取消read
+            auto read_res = co_await timeout(5000, socket.read_from(buf.data(), buf.size()));
+            if (!read_res) {
+                cout << "read timeout!\n";
+                continue;
+            }
 
-    // receive
-    array<char, 1024> buf;
-    for (; ;) {
-        auto [rdlen, addr] = co_await socket.read_from(buf.data(), buf.size());
-        channel->async_send({buf.data(), rdlen}, addr);
+            auto [len, address] = read_res.unwrap();
+            cout << string_view(buf.data(), len) << '\n';
+
+            auto write_res = co_await timeout(5000, socket.write_to(buf.data(), len, address));
+            if (!write_res) {
+                cout << "write timeout!\n";
+                continue;
+            }
+        }
+    } catch (const system_error& err) {
+        cout << err.what() << '\n';
     }
 }
 
