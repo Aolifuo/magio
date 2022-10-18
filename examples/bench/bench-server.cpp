fd85438424e4768d13250ac179c54b6e5afa5316
Mikasuki <2649856056@qq.com>
2022-10-18 21:00:12 +0800

    add: server for bench mark

--- examples/bench/bench-server.cpp
+++ examples/bench/bench-server.cpp
@@ -4,12 +4,40 @@
 using namespace std;
 using namespace magio;
 
+//  ./ab.exe -n 1000000 -c 1000 -k http://127.0.0.1:8000/
+// wrk
+
+const string HEAHER =
+    "HTTP/1.1 200 OK\r\n"
+    "Server: Asio\r\n"
+    "Accept: */*\r\n"
+    "Content-Type: text/html\r\n"
+    "Content-Length: 274\r\n"
+    "\r\n";
+
+const char HTML[] = 
+    R"(<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Hello world
+</body>
+</html>)";
+
+
+const string RESP = HEAHER + HTML;
+
 Coro<void> process(TcpStream stream) {
     try {
         char buf[1024];
         for (; ;) {
-            auto rdlen = co_await stream.read(buf, sizeof(buf));
-            co_await stream.write(buf, rdlen);
+            co_await stream.read(buf, sizeof(buf));
+            co_await stream.write(RESP.c_str(), RESP.length());
         }
     } catch (const runtime_error& e) {
         // cout << e.what() << '\n';
@@ -18,11 +46,10 @@ Coro<void> process(TcpStream stream) {
 
 Coro<void> amain(Magico& loop) {
     try {
-        auto server = co_await TcpServer::bind("127.0.0.1", 8000);
-        auto exe = co_await this_coro::executor;
+        auto server = co_await TcpServer::bind("0.0.0.0", 8000);
         for (; ;) {
             auto stream = co_await server.accept();
-            co_spawn(exe, process(std::move(stream)));
+            co_spawn(co_await this_coro::executor, process(std::move(stream)));
         }
     } catch (const runtime_error& e) {
         cout << e.what() << '\n';
@@ -30,7 +57,7 @@ Coro<void> amain(Magico& loop) {
 }
 
 int main() {
-    Magico loop(1);
+    Magico loop(16);
     co_spawn(loop.get_executor(), amain(loop));
     loop.run();
 }
