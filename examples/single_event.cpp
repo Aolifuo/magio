daffdb8b95d24102a373d9dc51b1f574b174aefc
Mikasuki <2649856056@qq.com>
2022-10-19 20:53:37 +0800

    add: example of single event

---
+++ examples/single_event.cpp
@@ -0,0 +1,25 @@
+#include "magio/magio.h"
+
+using namespace std;
+using namespace magio;
+
+Coro<> amain() {
+    auto exe = co_await this_coro::executor;
+    SingleEvent event(exe);
+
+    co_spawn(exe, [](SingleEvent& ev) -> Coro<> {
+        co_await sleep(1000);
+        ev.notify();
+    }(event));
+
+    co_await co_spawn(exe, [](SingleEvent& ev) -> Coro<> {
+        co_await ev.wait();
+        cout << "after wait\n";
+    }(event), use_coro);
+}
+
+int main() {
+    Magico loop;
+    co_spawn(loop.get_executor(), amain());
+    loop.run();
+}
