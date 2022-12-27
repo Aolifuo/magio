11faeca7a8d60e9b67a49460654f2d72c244cf95
Mikasuki <2649856056@qq.com>
2022-12-27 20:28:55 +0800

    example: fork join sort

--- README.md
+++ README.md
@@ -50,7 +50,7 @@ Coro<> server() {
 }
 
 int main() {
-    CoroContext ctx(100);
+    CoroContext ctx(128);
     this_context::spawn(server());
     ctx.start();
 }
@@ -97,7 +97,7 @@ Coro<> client() {
 }
 
 int main() {
-    CoroContext ctx(100);
+    CoroContext ctx(128);
     this_context::spawn(client());
     ctx.start();
 }
@@ -138,7 +138,7 @@ Coro<> amain() {
 }
 
 int main() {
-    CoroContext ctx(100);
+    CoroContext ctx(128);
     this_context::spawn(amain());
     ctx.start();    
 }
@@ -164,8 +164,58 @@ Coro<> copyfile() {
 }
 
 int main() {
-    CoroContext ctx(100);
+    CoroContext ctx(128);
     this_context::spawn(copyfile());
+    ctx.start();
+}
+```
+
+### fork join sort
+
+```cpp
+template<typename Iter, typename Pred>
+Coro<> fork_join_sort(ThreadPool& exe, Iter bg, Iter ed, Pred pred, size_t times) {
+    if (bg >= ed) {
+        co_return;
+    }
+
+    if (times > 0) {
+        auto mid = bg + (ed - bg) / 2;
+        nth_element(bg, mid, ed, pred);
+        co_await join(
+            fork_join_sort(exe, bg, mid, pred, times - 1),
+            fork_join_sort(exe, mid + 1, ed, pred, times - 1)
+        );
+    } else {
+        co_await exe.spawn_blocking([bg, ed, pred] {
+            sort(bg, ed, pred);
+        });
+    }
+    co_return;
+}
+
+Coro<> amain(ThreadPool& pool) {
+    vector<int> vec(1e8);
+    random_device dev;
+    default_random_engine eng(dev());
+    uniform_int_distribution<> uid;
+    for (auto& v : vec) {
+        v = uid(eng);
+    }
+    
+    auto bg = TimerClock::now();
+    co_await fork_join_sort(pool, vec.begin(), vec.end(), greater<>(), 3);
+    auto dif = TimerClock::now() - bg;
+    M_INFO("{}", dif);
+
+    co_return this_context::stop();
+}
+
+int main() {
+    CoroContext ctx(128);
+    ThreadPool pool(8);
+    ctx.spawn(amain(pool));
+    pool.start();
     ctx.start();
 }
 ```
--- examples/v3/v3-sort.cpp
+++ examples/v3/v3-sort.cpp
@@ -6,50 +6,40 @@ using namespace std;
 using namespace magio;
 using namespace chrono_literals;
 
-template<typename Iter>
-Coro<> fork_join_sort(ThreadPool& exe, Iter bg, Iter ed, size_t times) {
-    using Value = typename iterator_traits<Iter>::value_type;
+template<typename Iter, typename Pred>
+Coro<> fork_join_sort(ThreadPool& exe, Iter bg, Iter ed, Pred pred, size_t times) {
     if (bg >= ed) {
         co_return;
     }
-    Value first = *bg;
 
     if (times > 0) {
-        Iter it = partition(bg, ed, [first](Value& v) {
-            return v < first;
-        });
+        auto mid = bg + (ed - bg) / 2;
+        nth_element(bg, mid, ed, pred);
         co_await join(
-            fork_join_sort(exe, bg, it, times - 1),
-            fork_join_sort(exe, it + 1, ed, times - 1)
+            fork_join_sort(exe, bg, mid, pred, times - 1),
+            fork_join_sort(exe, mid + 1, ed, pred, times - 1)
         );
     } else {
-        co_await exe.spawn_blocking([bg, ed] {
-            sort(bg, ed);
+        co_await exe.spawn_blocking([bg, ed, pred] {
+            sort(bg, ed, pred);
         });
     }
     co_return;
 }
 
 Coro<> amain(ThreadPool& pool) {
-    vector<int> vec1(1e8);
+    vector<int> vec(1e8);
     random_device dev;
     default_random_engine eng(dev());
     uniform_int_distribution<> uid;
-
-    for (auto& v : vec1) {
+    for (auto& v : vec) {
         v = uid(eng);
     }
-
+    
     auto bg = TimerClock::now();
-    sort(vec1.begin(), vec1.end());
+    co_await fork_join_sort(pool, vec.begin(), vec.end(), greater<>(), 3);
     auto dif = TimerClock::now() - bg;
-    M_INFO("{}", chrono::duration_cast<chrono::milliseconds>(dif));
-
-    auto vec2 = vec1;
-    bg = TimerClock::now();
-    co_await fork_join_sort(pool, vec2.begin(), vec2.end(), 3);
-    dif = TimerClock::now() - bg;
-    M_INFO("{}", chrono::duration_cast<chrono::milliseconds>(dif));
+    M_INFO("{}", dif);
 
     co_return this_context::stop();
 }
