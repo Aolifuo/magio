fc8aaddeb185522e39702a9dae97d65d3a5bf7b2
Mikasuki <2649856056@qq.com>
2023-01-02 22:22:35 +0800

    example: ticker

---
+++ examples/v3/v3-ticker.cpp
@@ -0,0 +1,29 @@
+#include "magio-v3/magio.h"
+
+using namespace std;
+using namespace magio;
+using namespace chrono_literals;
+
+template<typename Rep, typename Per>
+void ticker(const chrono::duration<Rep, Per>& duration, size_t times, std::function<void()>&& func) {
+    this_context::expires_after(duration, [duration, times, func = std::move(func)](bool flag) mutable {
+        if (!flag) {
+            return;
+        }
+        func();
+        --times;
+        if (times != 0) {
+            ticker(duration, times, std::move(func));
+        } else {
+            this_context::stop();
+        }
+    });
+}
+
+int main() {
+    CoroContext ctx(128);
+    ticker(1s, 5, [] {
+        M_INFO("{}", "once");
+    });
+    ctx.start();
+}
