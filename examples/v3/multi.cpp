3b2f830f14cc4ea76804bdb824c8a27736bd3c77
Mikasuki <2649856056@qq.com>
2023-01-03 19:51:36 +0800

    example: multithreaded contexts

---
+++ examples/v3/multi.cpp
@@ -0,0 +1,54 @@
+#include "magio-v3/magio.h"
+
+using namespace std;
+using namespace magio;
+using namespace chrono_literals;
+
+Coro<> handle_connection(net::Socket sock) {
+    std::error_code ec;
+    char buf[1024];
+    for (; ;) {
+        size_t rd = co_await sock.receive(buf, sizeof(buf), ec);
+        if (ec || rd == 0) {
+            M_ERROR("{}", ec ? ec.message() : "EOF");
+            break;
+        }
+        M_INFO("receive: {}", string_view(buf, rd));
+        co_await sock.send(buf, rd, ec);
+        if (ec) {
+            M_ERROR("{}", ec.message());
+            break;
+        }
+    }
+}
+
+Coro<> server(MultithreadedContexts& ctxs) {
+    std::error_code ec;
+    net::EndPoint local(net::make_address("::1", ec), 1234);
+    if (ec) {
+        M_FATAL("{}", ec.message());
+    }
+
+    net::Acceptor acceptor;
+    acceptor.bind_and_listen(local, ec);
+    if (ec) {
+        M_FATAL("{}", ec.message());
+    }
+
+    for (; ;) {
+        auto [socket, peer] = co_await acceptor.accept(ec);
+        if (ec) {
+            M_ERROR("{}", ec.message());
+        }
+        M_INFO("accept [{}]:{}", peer.address().to_string(), peer.port());
+        ctxs.next_context().spawn(handle_connection(std::move(socket)));
+    }
+}
+
+int main() {
+    Logger::set_level(LogLevel::Fatal);
+    CoroContext ctx(50);
+    MultithreadedContexts threaded_ctxs(4, 50);
+    this_context::spawn(server(threaded_ctxs));
+    threaded_ctxs.start_all();
+}
