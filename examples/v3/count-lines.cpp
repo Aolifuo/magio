6074fa23ad9232acdb534ed328af6ba3ec3c8053
Mikasuki <2649856056@qq.com>
2023-01-06 12:12:20 +0800

    example: count lines

--- examples/v3/count-lines.cpp
+++ examples/v3/count-lines.cpp
@@ -7,61 +7,77 @@ using namespace chrono_literals;
 
 namespace fs = filesystem;
 
-Coro<> count_lines_impl(string path, Mutex& co_m, Condition& cond, size_t& lines, size_t& count_down) {
-    char buf[1024];
-    std::error_code ec;
-    File file(path.c_str(), File::ReadOnly);
-    if (!file) {
-        M_ERROR("cannot open {}", path);
-        co_return;
-    }
-    for (; ;) {
-        size_t len = co_await file.read(buf, sizeof(buf), ec);
-        if (ec || len == 0) {
-            break;
-        }
-        string_view sv(buf, len);
-        for (size_t pos = 0; (pos = sv.find('\n', pos)) != string_view::npos;) {
-            ++pos;
-            ++lines;
-        }
+class CountLines {
+public:
+    CountLines(const char* dir_name): dir_name_(dir_name)
+    { }
+
+    void start() {
+        this_context::spawn(count_all());
     }
 
-    --count_down;
-    if (0 == count_down) {
-        cond.notify_one();
-    }
-}
+private:
+    Coro<> count_all() {
+        size_t file_num = 0;
+        if (!fs::exists(dir_name_) || !fs::is_directory(dir_name_)) {
+            M_INFO("{} does not exit or is not dir", dir_name_);
+            co_return;
+        }
 
-Coro<> count_lines(const char* dir_name) {
-    size_t lines = 0;
-    size_t count_down = 0;
-    size_t file_num = 0;
-    Mutex co_m;
-    Condition cond;
-    if (fs::is_regular_file(dir_name)) {
-        count_down = 1;
-        this_context::spawn(count_lines_impl(dir_name, co_m, cond, lines, count_down));
-    } else {
-        for (const auto& entry : fs::recursive_directory_iterator(dir_name)) {
+        for (auto& entry : fs::recursive_directory_iterator(dir_name_)) {
             if (!entry.is_regular_file()) {
-                continue;
+                break;
             }
-            
-            ++count_down;
-            ++file_num;
-            this_context::spawn(count_lines_impl(entry.path().string(), co_m, cond, lines, count_down));
+
+            count_down_++;
+            this_context::spawn(count_one_file(entry.path().string()));
+        }
+
+        file_num = count_down_;
+        auto lock = co_await mutex_.lock_guard();
+        co_await cond_.wait(lock, [&] { return count_down_ == 0; });
+        M_INFO("file num: {}, lines: {}", file_num, lines_);
+        this_context::stop();
+    }
+
+    Coro<> count_one_file(string path) {
+        char buf[1024];
+        std::error_code ec;
+        File file(path.c_str(), File::ReadOnly);
+        if (!file) {
+            M_ERROR("cannot open {}", path);
+            co_return;
+        }
+
+        for (; ;) {
+            size_t len = co_await file.read(buf, sizeof(buf), ec);
+            if (ec || len == 0) {
+                break;
+            }
+            string_view sv(buf, len);
+            for (size_t pos = 0; (pos = sv.find('\n', pos)) != string_view::npos;) {
+                ++pos;
+                ++lines_;
+            }
+        }
+
+        --count_down_;
+        if (count_down_ == 0) {
+            cond_.notify_all();
         }
     }
 
-    auto lock = co_await co_m.lock_guard();
-    co_await cond.wait(lock, [&] { return 0 == count_down; });
-    M_INFO("file num: {}, lines: {}", file_num, lines);
-    co_return this_context::stop();
-}
+    Mutex mutex_;
+    Condition cond_;
+
+    string dir_name_;
+    size_t count_down_ = 0;
+    size_t lines_ = 0;
+};
 
 int main() {
     CoroContext ctx(128);
-    ctx.spawn(count_lines("path/to/dir"));
+    CountLines cl(R"(/path/to/dir)");
+    cl.start();
     ctx.start();
 }
