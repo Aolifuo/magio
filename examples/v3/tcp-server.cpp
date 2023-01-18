a973cfa225adbd66b1c3b74a60fa00e4dd257ebd
Mikasuki <2649856056@qq.com>
2023-01-18 19:14:05 +0800

    update

--- examples/v3/tcp-server.cpp
+++ examples/v3/tcp-server.cpp
@@ -45,28 +45,8 @@ Coro<> server() {
     }
 }
 
-template<typename Rep, typename Per>
-void ticker(const chrono::duration<Rep, Per>& duration, size_t times, std::function<void()>&& func) {
-    this_context::expires_after(duration, [duration, times, func = std::move(func)](bool flag) mutable {
-        if (!flag) {
-            return;
-        }
-        func();
-        --times;
-        if (times != 0) {
-            ticker(duration, times, std::move(func));
-        } else {
-            this_context::stop();
-        }
-    });
-}
-
-
 int main() {
     CoroContext ctx(128);
-    ticker(2s, 999, [] {
-        M_INFO("once");
-    });
     this_context::spawn(server());
     ctx.start();
 }
